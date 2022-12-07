59ca3b2da166b2b6df6c292bf0b7f6668218ac89
ntlmlnkv <melnikova.natalya.97@mail.ru>
2022-12-07 13:53:18 +0300

    first_commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://banner2.cleanpng.com/20180715/zio/kisspng-logo-font-flame-logo-5b4b2d7c3b73e0.2237387315316535002435.jpg'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://bstatic.com/data/xphoto/1182x887/202/20233869.jpg?size=S'></img>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>post1</div>
+            <div>post2</div>
+        </div>
+    </div>
+}
+
+export default Profile;
