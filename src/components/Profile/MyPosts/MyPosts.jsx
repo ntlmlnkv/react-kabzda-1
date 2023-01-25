969bd8560a9c3670f1b15914ab5528ebaa9a87e0
ntlmlnkv <melnikova.natalya.97@mail.ru>
2023-01-25 09:37:48 +0300

    31 less

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,14 +6,22 @@ const MyPosts = (props) => {
 
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />)
 
+    let newPostElement = React.createRef();
+
+
+let addPost =()=> {
+    let text = newPostElement.current.value;
+    alert(text)
+}
+
     return (<div className={s.postsBlock}>
         <h3> My posts </h3>
         <div>
             <div>
-                <textarea></textarea>
+                <textarea ref={newPostElement}></textarea>
             </div>
             <div>
-                <button>Add posts</button>
+                <button onClick ={addPost}>Add posts</button>
             </div>
         </div>
         <div className={s.posts}>
